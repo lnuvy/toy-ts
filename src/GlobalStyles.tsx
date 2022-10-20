dea7df05f012929939461f7743fd00c09c725f35
lnuvy <lnuvy.code@gmail.com>
2022-10-20 19:40:51 +0900

    emotion GlobalStyles

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Global, css } from "@emotion/react";
 
 /**
- * emotion 글로벌 스타일 적용법입니다.
+ * emotion 글로벌 스타일 적용법입니다
+ * 추가하실 글로벌 css는 여기서 추가하시면 됩니다
  */
 const GlobalStyles = () => {
   const styles = css`
@@ -12,7 +13,7 @@ const GlobalStyles = () => {
       margin: 0;
       font-family: Noto Sans;
       scroll-behavior: smooth;
-      background-color: #f5f5f5;
+      background-color: #fff;
     }
     a {
       color: inherit;
