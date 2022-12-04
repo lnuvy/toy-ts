2b538f78d19ea4d5432899dc5d212aee88e469ed
lnuvy <lnuvy.code@gmail.com>
2022-12-04 21:02:41 +0900

    app.tsx 주석 위아래로 바꾸면서 작업하기

--- src/App.tsx
+++ src/App.tsx
@@ -12,35 +12,51 @@ import { useState } from "react";
 import AuthLayout from "@pages/AuthLayout";
 import EditUserPage from "@pages/edituser-page/EditUserPage";
 
+// const App = () => {
+//   const [isLogin, setIsLogin] = useState(false);
+
+//   useEffect(() => {
+//     const session = getStorage();
+//     if (session) setIsLogin(true);
+//     else setIsLogin(false);
+//   }, []);
+
+//   if (isLogin)
+//     return (
+//       <AuthLayout>
+//         <Routes>
+//           <Route path="/" element={<LandingPage />} />
+//           <Route path="/project" element={<ProjectPage />} />
+//           <Route path="/project/:projectId" element={<ProjectDetail />} />
+//           <Route path="/sprint/:projectId" element={<SprintPage />} />
+//           <Route path="/edituser" element={<EditUserPage />} />
+//         </Routes>
+//       </AuthLayout>
+//     );
+
+//   return (
+//     <Routes>
+//       <Route path="/*" element={<LoginPage />} />
+//       <Route path="/register" element={<RegisterPage />} />
+//       <Route path="/check" element={<CheckPage />} />
+//     </Routes>
+//   );
+// };
+
+// export default App;
+
 const App = () => {
-  const [isLogin, setIsLogin] = useState(false);
-
-  useEffect(() => {
-    const session = getStorage();
-    if (session) setIsLogin(true);
-    else setIsLogin(false);
-  }, []);
-
-  if (isLogin)
-    return (
-      <AuthLayout>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/project" element={<ProjectPage />} />
-          <Route path="/project/:projectId" element={<ProjectDetail />} />
-          <Route path="/sprint/:projectId" element={<SprintPage />} />
-          <Route path="/edituser" element={<EditUserPage />} />
-        </Routes>
-      </AuthLayout>
-    );
-  else
-    return (
+  return (
+    <AuthLayout>
       <Routes>
-        <Route path="/*" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/check" element={<CheckPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/project" element={<ProjectPage />} />
+        <Route path="/project/:projectId" element={<ProjectDetail />} />
+        <Route path="/sprint/:projectId" element={<SprintPage />} />
+        <Route path="/edituser" element={<EditUserPage />} />
       </Routes>
-    );
+    </AuthLayout>
+  );
 };
 
 export default App;
