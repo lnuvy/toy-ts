6357aff9d134a1d38c61a24008be34b25f1d73fe
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2022-12-11 11:08:20 +0900

    페이지 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -4,59 +4,61 @@ import LoginPage from "@pages/login-page/LoginPage";
 import RegisterPage from "@pages/register-page/RegisterPage";
 import CheckPage from "@pages/check-page/CheckPage";
 import LandingPage from "@pages/landing-page/LandingPage";
-import SprintPage from "@pages/sprint-page/SprintPage";
+import SprintPage from "@pages/sprint-page/AddPage";
 import ProjectDetail from "@pages/project-detail";
 import { useEffect } from "react";
 import { getStorage } from "@utils/storage";
 import { useState } from "react";
 import AuthLayout from "@pages/AuthLayout";
 import EditUserPage from "@pages/edituser-page/EditUserPage";
-
-// const App = () => {
-//   const [isLogin, setIsLogin] = useState(false);
-
-//   useEffect(() => {
-//     const session = getStorage();
-//     if (session) setIsLogin(true);
-//     else setIsLogin(false);
-//   }, []);
-
-//   if (isLogin)
-//     return (
-//       <AuthLayout>
-//         <Routes>
-//           <Route path="/" element={<LandingPage />} />
-//           <Route path="/project" element={<ProjectPage />} />
-//           <Route path="/project/:projectId" element={<ProjectDetail />} />
-//           <Route path="/sprint/:projectId" element={<SprintPage />} />
-//           <Route path="/edituser" element={<EditUserPage />} />
-//         </Routes>
-//       </AuthLayout>
-//     );
-
-//   return (
-//     <Routes>
-//       <Route path="/*" element={<LoginPage />} />
-//       <Route path="/register" element={<RegisterPage />} />
-//       <Route path="/check" element={<CheckPage />} />
-//     </Routes>
-//   );
-// };
-
-// export default App;
+import EditProjectPage from "@pages/editproject-page/EditProjectPage";
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const session = getStorage();
+    if (session) setIsLogin(true);
+    else setIsLogin(false);
+  }, []);
+
+  if (isLogin)
+    return (
+      <AuthLayout>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/project" element={<ProjectPage />} />
+          <Route path="/project/:projectId" element={<ProjectDetail />} />
+          <Route path="/sprint/:projectId" element={<SprintPage />} />
+          <Route path="/edituser" element={<EditUserPage />} />
+          <Route path="/editproject/:projectId" element={<EditProjectPage />} />
+        </Routes>
+      </AuthLayout>
+    );
+
   return (
-    <AuthLayout>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/project" element={<ProjectPage />} />
-        <Route path="/project/:projectId" element={<ProjectDetail />} />
-        <Route path="/sprint/:projectId" element={<SprintPage />} />
-        <Route path="/edituser" element={<EditUserPage />} />
-      </Routes>
-    </AuthLayout>
+    <Routes>
+      <Route path="/*" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/check" element={<CheckPage />} />
+    </Routes>
   );
 };
 
 export default App;
+
+// const App = () => {
+//   return (
+//     <AuthLayout>
+//       <Routes>
+//         <Route path="/" element={<LandingPage />} />
+//         <Route path="/project" element={<ProjectPage />} />
+//         <Route path="/project/:projectId" element={<ProjectDetail />} />
+//         <Route path="/sprint/:projectId" element={<SprintPage />} />
+//         <Route path="/edituser" element={<EditUserPage />} />
+//       </Routes>
+//     </AuthLayout>
+//   );
+// };
+
+// export default App;
