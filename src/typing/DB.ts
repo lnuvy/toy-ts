8771221eb007ee97f65699056972d85b07707b54
seobi-97 <tkfkdrk12@skuniv.ac.kr>
2023-01-09 17:48:09 +0900

    sprint, joblist 타입 설정

--- src/typing/DB.ts
+++ src/typing/DB.ts
@@ -10,11 +10,21 @@ export interface SprintType {
   sprintId: number;
   sprintName: string;
   sprintDetail: string;
-  localDateTime: Date; // TODOS: type 다를 수 있음, response 모양 보고 변경하기
+  localDateTime: string; // TODOS: type 다를 수 있음, response 모양 보고 변경하기
+  type: number;
 }
 
 export interface MemberType {
   email: string;
   name: string;
   userId: number;
+}
+
+export interface JobListType {
+  jobId: string;
+  toDo: string;
+  detail: string;
+  start: Date;
+  end: Date;
+  jobWorkers: object;
 }
